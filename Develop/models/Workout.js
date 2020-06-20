3829a3ada67636c2b9b1618e85abcff50a1330f7
Mwansa <Mwansad.mwango@gmail.com>
2020-06-21 00:47:47 +0800

    fixed model

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -3,17 +3,35 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  day: {
-    type: Date
-  },
-  excercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Excercise"
-    }
-  ]
-});
+  
+          day: { type: Date, default: Date.now },
+          exercises: [{
+              type: {type: String, required: true},
+              name: {type: String, required: true},
+              distance: Number,
+              duration: Number, 
+              weight: Number,
+              reps: Number,
+              sets: Number
+          }]
+      },
+     
+      {
+          toJSON: {
+            virtuals: true
+          }
+      },
+     
+      {
+          versionKey: false
+      }
+  );
+
+  WorkoutSchema.virtual("totalDuration").get(function () {
+      return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+      }, 0);
+  });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Excercise: require("./Excercise"),
+  Exercise: require("./Exercise"),
   Workout: require("./Workout")
 };
