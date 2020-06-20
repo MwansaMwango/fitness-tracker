1dd2fe4d0cf15a7f6e0f5671e5ff657e35ede47a
Mwansa <Mwansad.mwango@gmail.com>
2020-06-21 00:53:43 +0800

    fixed typos

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -9,34 +9,30 @@ const db = require("../models");
 
 module.exports = function (app) {
 
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // ---------------------------------------------------------------------------
-
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
       .then(dbWorkout => {
         res.json(dbWorkout);
+        console.log(res.json(dbWorkout));
       })
       .catch(err => {
         res.json(err);
       });
   });
 
-  app.post("/api/workouts/:id", (req, res) => {
-    req.body._id = req.params.id;
-    db.Exercise.create(req.body)
-      .then( ({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+  app.put("/api/workouts/:id", (req, res) => {
+       db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body }})
       .then(dbWorkout => {
         res.json(dbWorkout);
       })
+
       .catch(err => {
         res.json(err);
       });
   });
 
-  app.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
       .then(dbWorkout => {
         res.json(dbWorkout);
       })
@@ -46,78 +42,13 @@ module.exports = function (app) {
   });
 
   app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-      .populate("exercises")
-      .then(dbWorkout => {
+    db.Workout.find()
+        .then(dbWorkout => {
         res.json(dbWorkout);
       })
       .catch(err => {
         res.json(err);
       });
-  });
-
-  //-----------------Boiler code---------------------
-
-    app.post("/submit", (req, res) => {
-    console.log(req.body);
-  
-    db.notes.insert(req.body, (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    });
-  });
-  
-  app.get("/all", (req, res) => {
-    db.notes.find({}, (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.json(data);
-      }
-    });
-  });
-  
-  app.get("/find/:id", (req, res) => {
-    db.notes.findOne(
-      {
-        _id: mongojs.ObjectId(req.params.id)
-      },
-      (error, data) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(data);
-        }
-      }
-    );
-  });
-  
-  app.delete("/delete/:id", (req, res) => {
-    db.notes.remove(
-      {
-        _id: mongojs.ObjectID(req.params.id)
-      },
-      (error, data) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(data);
-        }
-      }
-    );
-  });
-  
-  app.delete("/clearall", (req, res) => {
-    db.notes.remove({}, (error, response) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(response);
-      }
-    });
   });
 
 };
