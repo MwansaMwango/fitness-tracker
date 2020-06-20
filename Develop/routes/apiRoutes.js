5ea0fb005c351a354170f51893d3544e544e4aed
Mwansa <Mwansad.mwango@gmail.com>
2020-06-20 23:47:08 +0800

    Fixed static routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,61 +1,123 @@
 // ===============================================================================
 // DEPENDENCIES
 // ===============================================================================
-const path = require("path");
-const util = require("util");
 
+const db = require("../models");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
-  let notesList = [];
-
-  function saveNotesList(newNotesList) {
-    //Check if folder exists
-    fs.existsSync(dBPath_dir) || fs.mkdirSync(dBPath_dir);
-    // Create output file
-    writeFileAsync(
-      dBPath,
-      JSON.stringify(newNotesList),
-      { flag: "w" },
-      (err) => {
-        if (err) throw err;
-        console.log("Succesfully saved new note");
-      }
-    );
-  }
 
   // API GET Requests
   // Below code handles when users "visit" a page.
   // ---------------------------------------------------------------------------
 
-  app.get("/api/notes", function (req, res) {
-    // Should return the `db.json` file and return all saved notes as JSON.
-    res.send(readNotesList());
-    res.end;
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  //------------------------------------------------------------------------
-
-  app.post("/api/notes", function (req, res) {
-    let noteId = uuidv4();
-    let newNote = req.body;
-    newNote.id = noteId;
-    notesList = readNotesList();
-    notesList.push(newNote);
-    saveNotesList(notesList);
-    res.json(true);
+  app.post("/api/workouts/:id", (req, res) => {
+    req.body._id = req.params.id;
+    db.Exercise.create(req.body)
+      .then( ({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
 
-  app.delete("/api/notes/:id", function (req, res) {
-    let noteId = req.params.id;
-    noteList = readNotesList();
-    const updatedNotesList = notesList.filter((note) => note.id !== noteId);
-
-    saveNotesList(updatedNotesList);
-    res.send("Deleted");
+  app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
+
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .populate("exercises")
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  //-----------------Boiler code---------------------
+
+    app.post("/submit", (req, res) => {
+    console.log(req.body);
+  
+    db.notes.insert(req.body, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    });
+  });
+  
+  app.get("/all", (req, res) => {
+    db.notes.find({}, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
+  
+  app.get("/find/:id", (req, res) => {
+    db.notes.findOne(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      (error, data) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  });
+  
+  app.delete("/delete/:id", (req, res) => {
+    db.notes.remove(
+      {
+        _id: mongojs.ObjectID(req.params.id)
+      },
+      (error, data) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  });
+  
+  app.delete("/clearall", (req, res) => {
+    db.notes.remove({}, (error, response) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(response);
+      }
+    });
+  });
+
 };
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -14,8 +14,12 @@ module.exports = function (app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
   // If no matching route is found default to home
